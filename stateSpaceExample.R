60a896c7d899a809f79fd453bd91b80548ca0982
rhinophile <bijan.text@gmail.com>
2016-10-04 13:10:28 -0400

    fixed the example

--- stateSpaceExample.R
+++ stateSpaceExample.R
@@ -4,17 +4,17 @@ source('stateSpaceModel.R')
 source('stateSpacePlot.R')
 source('~/Projects/procVisData/dataViz.R')
 source('~/Projects/procVisData/bayesianFunctions.R')
-par(mfrow=c(3,2))
+par(mfrow=c(2,2))
 
 # set.seed(2016)
 
-ssSim <- ssSimulations(nSites = 100, #number of sites
-                       nTSet = 20, #number of Time steps
-                       beta = c(.7, -.3, .1, -.1, -.4, .2), #beta coefficients
-                       sig = .05, # process error
-                       tau = .2, # observation error
-                       plotFlag = T, # whether plot the data or not
-                       miss = .9, #fraction of missing data
+ssSim <- ssSimulations(nSites = 8, #number of sites
+                       nTSet = 10, #number of Time steps
+                       beta = c(3,2, -2, -1), #beta coefficients
+                       sig = .1, # process error
+                       tau = .3, # observation error
+                       plotFlag = F, # whether plot the data or not
+                       miss = .3, #fraction of missing data
                        TRUNC = F # trunctaed model or not
 )
 
@@ -23,10 +23,10 @@ ssOut <- stateSpace(x = ssSim$x, #predictors
                     tauPriorMean = NULL, 
                     tauPriorStrength = NULL, 
                     sigPriorMean = ssSim$sig,
-                    sigPriorStrength = 1,
+                    sigPriorStrength = 2,
                     connect = ssSim$connect, # connectivity of time data
-                    ng = 2000, #number of gibbs sampling
-                    burnin = 1000, #numbe of burning
+                    ng = 10000, #number of gibbs sampling
+                    burnin = 5000, #numbe of burning
                     MPstep = .1,  # MetroPolis-Hasting initial step size  
                     nAccept = 30,  # number of steps to check the acceptance rate
                     storeLatent = T, # whether store latent state or not
@@ -42,15 +42,18 @@ ssGibbs <- data.frame(beta=ssOut$bgibbs,
                       sigma=ssOut$sgibbs,
                       tau=ssOut$tgibbs)
 #ssGibbs <- ssGibbs[-(1:ssOut$nBurnin),]
+trueValues <- c(ssSim$beta, ssSim$sig, ssSim$tau)
+yRange <- range(ssGibbs[-(1:ssOut$nBurnin),])
 
-plotGibbsDensity(outGibbs =  ssGibbs, burnin = ssOut$nBurnin, 
-          trueValues = c(ssSim$beta, ssSim$sig, ssSim$tau),
-          yRange = c(0,15))
+# plotGibbsDensity(outGibbs =  ssGibbs, burnin = ssOut$nBurnin, 
+#           trueValues = trueValues,
+#           yRange = c(0,15))
 
-plotGibbsChains(outGibbs =  ssGibbs, burnin = ssOut$nBurnin, 
-                 trueValues = c(ssSim$beta, ssSim$sig, ssSim$tau))
+plotGibbsChains(outGibbs =  ssGibbs, colPalette = colorRampPalette(topo.colors(ncol(ssGibbs))),# burnin = ssOut$nBurnin, 
+                 trueValues = trueValues)
 
-plotGibbsBoxplots(outGibbs =  ssGibbs, burnin = ssOut$nBurnin, sort = F, textAdj = 0, trueValues = c(ssSim$beta, ssSim$sig, ssSim$tau), vert = F)
+plotGibbsBoxplots(outGibbs =  ssGibbs, burnin = ssOut$nBurnin, sort = F, textAdj = 0, sigPlot = F,
+                  trueValues = trueValues, vert = T, rangeY = range(c(trueValues, yRange)))
 
 stateSpaceTemporalPost(ssSim$x, ssSim$y, 
                        beta = ssOut$bgibbs,
